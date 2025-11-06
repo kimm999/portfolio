4cc2d043e74bf6376ad14740222ab55fd2337132
kimm999 <sosorry552@gmail.com>
2025-11-06 21:24:17 +0900

    배포설정정

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio/', // GitHub repository 이름으로 변경하세요 (예: '/your-repo-name/')
 })
 
